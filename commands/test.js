94748c46e33adae6e34a122fdb258c99603653f8
TurtlePaw <turtlesimsyt@gmail.com>
2022-01-31 15:11:46 -0500

    Change test to an embed

--- commands/test.js
+++ commands/test.js
@@ -1,11 +1,16 @@
 const discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const Embed = require("../Embed");
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("test")
     .setDescription("test command"),
     async execute(interaction, client){
-        await interaction.reply("test or something idek")
+        await interaction.reply({
+            embeds: new Embed()
+            .setDescription(`Test or something idek idrk.`)
+            .build()
+        });
     }
 }
